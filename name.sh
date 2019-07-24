d7a66376a300557190355b6e5e821cc558f0d10e
alsantarese <45601720+alsantarese@users.noreply.github.com>
2019-07-24 22:52:06 +0200

    Add files via upload

---
+++ name.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# colors
+r='\033[1;31m'
+g='\033[1;32m'
+y='\033[1;33m'
+b='\033[1;34m'
+p='\033[1;35m'
+c='\033[1;36m'
+w='\033[1;37m'
+clear
+echo -e "$r     please Whait "
+sleep 1
+apt list --upgradable
+dpkg --configure -a
+pkg install figlet -y
+pkg install toilet -y
+clear
+echo -e "$y    Done Thank You ^_+ ♥️😁"
+sleep 2
+
+clear
+read -p "Enter Name~: " joker
+
+toilet -f mono12 -F gay $joker
+echo -e "$r"
+figlet -f mono12 $joker
+echo -e "$g"
+figlet -f big $joker
+echo -e "$y"
+figlet -f small $joker
+toilet -f mono12 -F gay $joker
+echo -e "$g"
+figlet -f mono12 $joker                               
+echo -e "$w"
+figlet -f big $joker
+echo -e "$c"
+figlet -f small $joker
+toilet -f mono12 -F gay $joker
+echo -e "$p"
+figlet -f mono12 $joker
+echo -e "$b"
+figlet -f big $joker
+echo -e "$y"
+figlet -f small $joker
+toilet -f mono12 -F gay $joker
+echo -e "$b"
+figlet -f mono12 $joker
+echo -e "$g"
+figlet -f big $joker
+echo -e "$c"
+figlet -f small $joker
+toilet -f mono12 -F gay $joker
+echo -e "$y"
+figlet -f mono12 $joker
+echo -e "$b"
+figlet -f big $joker
+echo -e "$y"
+figlet -f small $joker
+toilet -f mono12 -F gay $joker
+echo -e "$r"
+figlet -f mono12 $joker
+echo -e "$c"
+figlet -f big $joker
+echo -e "$b"
+figlet -f small $joker
+toilet -f mono12 -F gay $joker
+echo -e "$c"
+figlet -f mono12 $joker
+echo -e "$p"
+figlet -f big $joker
+echo -e "$y"
+figlet -f small $joker
+toilet -f mono12 -F gay $joker
+echo -e "$p"
+figlet -f mono12 $joker
+echo -e "$c"
+figlet -f big $joker
+echo -e "$w"
+figlet -f small $joker
